00e3c0a329bfa7a9eb09d9a67226510134de6508
Musharaf Aahel <49841802+musharafaahel@users.noreply.github.com>
2021-01-30 12:01:46 +0530

    Week-2

---
+++ temperature.cpp
@@ -0,0 +1,10 @@
+#include<simplecpp>
+
+main_program{
+    double centigrade, fahrenheit;
+    cout<<"Enter the temperature in centigrade: ";
+    cin>>centigrade;
+    fahrenheit=centigrade*9/5+32;
+    cout<<"In Fahrenheit : "<<fahrenheit;
+
+}
